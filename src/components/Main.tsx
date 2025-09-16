364382f2f534ad27ef7bf8622008221ea2e41c8b
Shasedharan Sekaran <55860434+shasedharan@users.noreply.github.com>
2025-09-15 22:38:34 -0400

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -14,14 +14,14 @@ function Main() {
         <div className="content">
           <div className="social_icons">
             <a href="https://github.com/shasedharan" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/shasedharan" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://www.linkedin.com/in/shasedharan/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
           <h1>Shasedharan Sekaran</h1>
           <p>Software Engineer</p>
 
           <div className="mobile_social_icons">
             <a href="https://github.com/shasedharan" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/shasedharan" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://www.linkedin.com/in/shasedharan/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
         </div>
       </div>
