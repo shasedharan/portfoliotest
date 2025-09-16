28d519102886e3a1144eb13a2426d80497339f4d
Shasedharan Sekaran <55860434+shasedharan@users.noreply.github.com>
2025-09-15 22:24:43 -0400

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -16,7 +16,7 @@ function Main() {
             <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
             <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
-          <h1>Yuji Sato</h1>
+          <h1>Yuji Sat</h1>
           <p>Full Stack Engineer</p>
 
           <div className="mobile_social_icons">
