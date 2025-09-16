a8dc42702602b1d9ac0faf5cca3cad207b12556d
Shasedharan Sekaran <55860434+shasedharan@users.noreply.github.com>
2025-09-15 22:27:13 -0400

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+himport React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import '../assets/styles/Main.scss';
@@ -16,8 +16,8 @@ function Main() {
             <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
             <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
-          <h1>Yuji Sat</h1>
-          <p>Full Stack Engineer</p>
+          <h1>Shasedharan Sekaran</h1>
+          <p>Software Engineer</p>
 
           <div className="mobile_social_icons">
             <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
