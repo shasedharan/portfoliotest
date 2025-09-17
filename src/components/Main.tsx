c8746b0d0213bd8eb01381ae19199c9ff7ccd226
Shasedharan Sekaran <55860434+shasedharan@users.noreply.github.com>
2025-09-16 20:37:23 -0400

    profile pic changes

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import '../assets/styles/Main.scss';
+import profilepic from '../assets/images/4M9A5836.jpg';
 
 function Main() {
 
@@ -9,7 +10,7 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
+          <img src={profilepic} alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
