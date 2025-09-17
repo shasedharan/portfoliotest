b0e0f209f5552303073b823f20c12bc890a2d43c
Shasedharan Sekaran <55860434+shasedharan@users.noreply.github.com>
2025-09-16 20:45:24 -0400

    update profile pic

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import '../assets/styles/Main.scss';
-import profilepic from '../assets/images/4M9A5836.jpg';
+import profilepic from '../assets/images/profilepic.png';
 
 function Main() {
 
