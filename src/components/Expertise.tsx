60e435c4e88826e380266d2af4b5aee03bd0113c
Shasedharan Sekaran <55860434+shasedharan@users.noreply.github.com>
2025-10-07 16:46:53 -0400

    testing new icon

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faReact, faDocker, faPython } from '@fortawesome/free-brands-svg-icons';
+import { ServerIcon, RocketLaunchIcon, ShieldCheckIcon } from "@heroicons/react/24/solid";
 import Chip from '@mui/material/Chip';
 import '../assets/styles/Expertise.scss';
 
@@ -48,7 +49,7 @@ function Expertise() {
             <h1>Expertise</h1>
             <div className="skills-grid">
                 <div className="skill">
-                    <FontAwesomeIcon icon={faReact} size="3x"/>
+                    <CodeBracketIcon icon={ServerIcon} className="w-12 h-12 text-accent"/>
                     <h3>Full Stack Web Development</h3>
                     <p>I have built a diverse array of web applications from scratch using modern technologies such as React and Flask. I have a strong proficiency in the SDLC process and frontend + backend development.</p>
                     <div className="flex-chips">
